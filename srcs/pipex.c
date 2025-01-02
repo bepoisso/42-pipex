0230c8ce21ae561cd9fed716d6a5562acc71011b
bepoisso <poisson.benjamin69@gmail.com>
2025-01-02 13:11:45 +0100

    Add handle_args

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: bepoisso <bepoisso@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 15:25:57 by bepoisso          #+#    #+#             */
-/*   Updated: 2025/01/02 11:51:31 by bepoisso         ###   ########.fr       */
+/*   Updated: 2025/01/02 13:11:20 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,97 @@ void	check_files(char *infile, char *outfile)
 	close(fd);
 }
 
-void	check_cmds(char *cmd1, char *cmd2, char **envp)
+char *get_env_value(char **envp, char *key)
 {
-	// Check si les commandes existe.
-		// si on renvois un message d'error
+	int key_len;
 	
+	key_len = ft_strlen(key);
+	while (*envp)
+	{
+		if (ft_strncmp(*envp, key, key_len) == 0 && (*envp)[key_len] == '=')
+			return (*envp + key_len + 1);
+		envp++;
+	}
+	return NULL;
+}
+
+char **get_paths(char *big_paths, char *cmd)
+{
+	char	**splited_path;
+	char	*temp;
+	int		i;
+
+	splited_path = ft_split(big_paths, ':');
+	i = 0;
+	while (splited_path[i])
+	{
+		temp = ft_strjoin(splited_path[i], "/");
+		free(splited_path[i]);
+		splited_path[i] = ft_strjoin(temp, cmd);
+		free(temp);
+		i++;
+	}
+	return (splited_path);
+}
+
+char	*get_path_cmd(char *cmd, char **envp)
+{
+	int		i;
+	char	**paths;
+	char	*result;
+
+	i = 0;
+	paths = get_paths(get_env_value(envp, "PATH"), cmd);
+	result = NULL;
+	while (paths[i])
+	{
+		if (access(paths[i], X_OK) == 0)
+		{
+			result = ft_strdup(paths[i]);
+			break ;
+		}
+		else
+			i++;
+	}
+	free_2d(paths);
+	if (!result)
+		return (ft_strdup(""));
+	retrun (result);
+}
+
+void	check_cmds(char *cmd_one, char *cmd_two, char **envp)
+{
+	char	*cmd_path_one;
+	char	*cmd_path_two;
+
+	cmd_path_one = get_path_cmd(cmd_one, envp);
+	cmd_path_two = get_path_cmd(cmd_two, envp);
+
+	if (!cmd_path_one)
+	{
+		free(cmd_path_one);
+		free(cmd_path_two);
+		ft_perror("Error\nFail to find cmd1\n", 1);
+	}
+	if (!cmd_path_two)
+	{
+		free(cmd_path_one);
+		free(cmd_path_two);
+		ft_perror("Error\nFail to find cmd2\n", 1);
+	}
 }
 
 void	check_args(int ac, char **av, char **envp)
 {
-	// Essayer d'ouvrir les fichier
 	if (ac != 5)
 		ft_perror("Error\nNeed 4 arguments\n", 1);
 	check_files(av[1], av[4]);
 	check_cmds(av[2], av[3], envp);
+}
+
+void	pipex(char *infile, char *cmd1, char *cmd2, char *outfile)
+{
+	
 }
 
 int main(int ac, char **av, char **envp)
